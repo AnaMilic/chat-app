388458553947193c3a71a40519a8892b4b9091b6
AnaMilic <76072963+AnaMilic@users.noreply.github.com>
2024-02-20 13:49:31 +0100

    Updated index.js

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,12 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-//const createServer=require("http");
-//import { createServer } from "http";
+
+const users = require("./routes/users");
+const chats = require("./routes/chats");
+const messages = require("./routes/messages");
 
 const app = express();
-//const server = createServer(app);
 
 dotenv.config();
 const PORT = process.env.PORT || 5050;
@@ -17,10 +18,6 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-const users = require("./routes/users");
-const chats = require("./routes/chats");
-const messages = require("./routes/messages");
 
 mongoose
   .connect("mongodb+srv://admin:admin@cluster0.1b64ygn.mongodb.net/", {
@@ -37,7 +34,6 @@ app.use("/api/messages", messages);
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
-app.listen(5050, () => {
+const server = app.listen(5050, () => {
   console.log(`Backend is running on port 5050`);
 });
-//server.listen(PORT, () => console.log("Server is running on port", PORT));
